2fc19565d96e5e300d9ffef818cf77f91301ba76
Ruwan Egodawatte <ruwanego@gmail.com>
2020-09-30 14:07:58 +0530

    Initial Commit

---
+++ cmd/version_test.go
@@ -0,0 +1,10 @@
+package cmd
+
+import "testing"
+
+func TestVersion(t *testing.T) {
+	showVersion()
+
+	// Output:
+	// go-fountain version: 0.0.1
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/ruwanego/go-fountain/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
